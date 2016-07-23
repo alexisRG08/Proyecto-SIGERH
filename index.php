a356db7e26f730ea2f9cf1361adb514c783a6623
hmorales117 <henry_moralesc@hotmail.com>
2016-07-23 13:14:05 -0500

    elimine el msj yt
    se elimino

--- index.php
+++ index.php
@@ -126,7 +126,7 @@ function ver(){
             </div>
              <div class="row">
             	<div class="col-xs-6">
-                	<p><a href="http://www.utcancun.edu.mx" target="_blank">Universidad Tecnologica de Cancun</a></p>
+                	
                 </div>
                 <div class="col-xs-6">
                 	<ul class="list-inline text-right">
