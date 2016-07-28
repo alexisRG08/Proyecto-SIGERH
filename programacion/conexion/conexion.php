62b0949be130f2bb2a23b8ebcba5aa969191a006
hmorales117 <henry_moralesc@hotmail.com>
2016-07-27 19:49:13 -0500

    pass y user

--- programacion/conexion/conexion.php
+++ programacion/conexion/conexion.php
@@ -1,12 +1,7 @@
 <?php
 define("host","localhost");
 define("usuario","root");
-<<<<<<< HEAD
+
 define("contrasenia", "");
 define("nombre_basedatos","recursosh");
-=======
-define("contrasenia", "root");
-define("nombre_basedatos","mydb");
->>>>>>> origin/master
-
 ?>
