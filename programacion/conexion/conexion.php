ed40f6374ef459deb0102556c14d8cefd73edab8
hmorales117 <henry_moralesc@hotmail.com>
2016-08-10 02:08:46 -0500

    cone

--- programacion/conexion/conexion.php
+++ programacion/conexion/conexion.php
@@ -2,8 +2,8 @@
 define("host","localhost");
 define("usuario","root");
 
-define("contrasenia", "root");
-define("nombre_basedatos","rhumanos");
+define("contrasenia", "");
+define("nombre_basedatos","recursoshumanos");
 /*
 define("contrasenia", "root");
 define("nombre_basedatos","rhumanos");
